4f31fbd9eda1855574d72f30b9fa61bab8da92f9
Marc Torra <marc@prodegee.com>
2025-05-13 05:00:53 -0300

    chore: integrate React Query provider and DevTools

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 // src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './index.css';
 import App from './App';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
